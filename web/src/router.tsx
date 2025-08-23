b5435673f6ca6a348e8403dee3ba6fbe20516103
Smnthjm08 <sumanthjmm.04@gmail.com>
2025-08-23 20:35:47 +0530

    fix: github actions

--- web/src/router.tsx
+++ web/src/router.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import { ConfirmationPage } from "./components/confirmation";
 import App from "./App";
-import LoginPage from "./pages/login-page";
-import RegisterPage from "./pages/register-page";
+import LoginPage from "./pages/auth/login-page";
+import RegisterPage from "./pages/auth/register-page";
 
 const confirmationEndpoint = "/confirm/:token";
 const loginEndpoint = "/login";
