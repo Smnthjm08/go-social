2bcb84f2fcad1bf035f05af5f26c6744becc083d
Smnthjm08 <sumanthjmm.04@gmail.com>
2025-08-23 20:24:19 +0530

    frontend: auth forms init and ci fixed

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,29 +1,14 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { ConfirmationPage } from "./components/confirmation.tsx";
+import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import { Toaster } from "@/components/ui/sonner";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/confirm/:token",
-    element: <ConfirmationPage />,
-  },
-]);
+import { router } from "./router.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <RouterProvider router={router} />
       <Toaster />
       {/* <App /> */}
     </ThemeProvider>
-  </StrictMode>
 );
